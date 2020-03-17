c135fa7c01a48a0a418f74e0409c3438dfe70e64
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-03-17 13:25:55 +0100

    Comparing readline-sync and prompt

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
 const reader = require("readline-sync")
-
 const username = reader.question('Username: ')
 console.log(username)
+
+// testing with prompt
+const prompt = require('prompt')
+prompt.start()
+const schema = {
+  properties: {
+    name: {
+      description: 'Username: ',
+      required: true
+    },
+    age: {
+      description: 'Age: ',
+      required: false
+    }
+  }
+}
+prompt.get(schema, function (err, result) {
+    if (err) { return onErr(err) }
+    Object.keys(schema.properties).forEach(key => {
+      console.log(`${key}: ${result[key]}`)
+    })
+})
+function onErr(err) {
+    console.log(err)
+    return 1
+}
