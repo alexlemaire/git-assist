1a9ff7d03464c7182b0ae5c167e629746e735563
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-03-18 15:08:57 +0100

    Refactored config to help maintainability

--- functions/config.js
+++ functions/config.js
@@ -1,32 +1,44 @@
-const promptly = require('promptly')
-const childProc = require('child_process')
-const git = require('isomorphic-git')
-const fs = require('fs')
-
 module.exports = async (args) => {
-  const fname = await promptly.prompt('First name: ')
-  const lname = await promptly.prompt('Last name: ')
-  const email = await promptly.prompt('Email: ')
+  const info = await infoPrompter()
   if (args[0] === '-g') {
-    // for global config we cannot use isomorphic-git as they do not currently support global config. May be worth helping them with this
-    childProc.execSync(`git config --global user.name "${fname} ${lname}"`)
-    childProc.execSync(`git config --global user.email "${email}"`)
+    setGlobalConfig(info)
   } else {
-    const params = [{
-      path: 'user.name',
-      value: `${fname} ${lname}`
-    },
-    {
-      path: 'user.email',
-      value: email
-    }]
-    for (const param of params) {
-      await git.setConfig({
-        fs,
-        dir: '.',
-        path: param.path,
-        value: param.value
-      })
-    }
+    await setLocalConfig(info)
+  }
+}
+
+async function infoPrompter() {
+  const promptly = require('promptly')
+  return {
+    name: `${await promptly.prompt('First name: ')} ${await promptly.prompt('Last name: ')}`,
+    email: await promptly.prompt('Email: ')
+  }
+}
+
+function setGlobalConfig(info) {
+  const childProc = require('child_process')
+  // for global config we cannot use isomorphic-git as they do not currently support global config. May be worth helping them with this
+  childProc.execSync(`git config --global user.name "${info.name}"`)
+  childProc.execSync(`git config --global user.email "${info.email}"`)
+}
+
+async function setLocalConfig(info) {
+  const git = require('isomorphic-git')
+  const fs = require('fs')
+  const params = [{
+    path: 'user.name',
+    value: info.name
+  },
+  {
+    path: 'user.email',
+    value: info.email
+  }]
+  for (const param of params) {
+    await git.setConfig({
+      fs,
+      dir: '.',
+      path: param.path,
+      value: param.value
+    })
   }
 }
