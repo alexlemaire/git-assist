8ef3f4d28528b8e754a66de32711f90d441e878e
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-03-18 18:29:00 +0100

    Added action for automated NPM publish

--- functions/auto-pull/index.js
+++ functions/auto-pull/index.js
@@ -1,7 +1,7 @@
 const promptly = require('promptly')
 
 module.exports = async (args) => {
-  require('../helpers/check-logs.js')()
+  require('./helpers/check-logs.js')()
   // TODO: should allow configuration via --config flag
   const name = await promptly.prompt('Name: ')
   const age = await promptly.prompt('Age: ')
