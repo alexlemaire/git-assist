58b2c8711061957314886c5ffa9e7b3b69534fea
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-03-23 19:02:11 +0100

    Improved user instructions with online instructions

--- functions/generate-gpg/helpers/user-info.js
+++ functions/generate-gpg/helpers/user-info.js
@@ -1,7 +1,33 @@
-module.exports = (gpgKey) => {
+const path = require('path')
+const staticRoot = path.join(__dirname, '..', 'static')
+
+module.exports = async (gpgKey) => {
+  const fs = require('fs')
+  logInstructions(gpgKey)
+  const keyPath = `${staticRoot}/key`
+  fs.writeFileSync(keyPath, gpgKey, 'utf-8')
+  await onlineInstructions()
+  fs.unlinkSync(keyPath)
+}
+
+function logInstructions (gpgKey) {
   consola.info('In order to add your new GPG key on GitHub:')
   console.log('  1. please follow this link: https://github.com/settings/keys')
   console.log('  2. click on "New GPG key"')
   console.log('  3. give a meaningful title to your new key and paste the content below into the "Key" field:\n')
   console.log(gpgKey)
+}
+
+async function onlineInstructions () {
+  const spawnSync = require('child_process').spawnSync
+  const promptly = require('promptly')
+  spawnSync('xdg-open', ['https://github.com/settings/keys'])
+  spawnSync('xdg-open', [`${staticRoot}/index.html`])
+  function validator(value) {
+    if (!value) {
+      throw new Error('Please follow the instructions to add your GPG key to GitHub')
+    }
+    return value
+  }
+  await promptly.confirm('Did you follow all given instructions and successfully added your GPG key (y/n)?', {validator})
 }
--- functions/generate-ssh/helpers/user-info.js
+++ functions/generate-ssh/helpers/user-info.js
@@ -1,8 +1,34 @@
-module.exports = (path) => {
-  const fs = require('fs')
+const fs = require('fs')
+const path = require('path')
+const staticRoot = path.join(__dirname, '..', 'static')
+
+module.exports = async (path) => {
+  const key = fs.readFileSync(`${path}.pub`, 'utf-8')
+  logInstructions(key)
+  const keyPath = `${staticRoot}/key`
+  fs.writeFileSync(keyPath, key, 'utf-8')
+  await onlineInstructions()
+  fs.unlinkSync(keyPath)
+}
+
+function logInstructions (key) {
   consola.info('In order to add your new SSH key on GitHub:')
   console.log('  1. please follow this link: https://github.com/settings/keys')
   console.log('  2. click on "New SSH key" on the top right corner')
   console.log('  3. give a meaningful title to your new key and paste the content below into the "Key" field:\n')
-  console.log(fs.readFileSync(`${path}.pub`, 'utf-8'))
+  console.log(key)
+}
+
+async function onlineInstructions () {
+  const spawnSync = require('child_process').spawnSync
+  const promptly = require('promptly')
+  spawnSync('xdg-open', ['https://github.com/settings/keys'])
+  spawnSync('xdg-open', [`${staticRoot}/index.html`])
+  function validator(value) {
+    if (!value) {
+      throw new Error('Please follow the instructions to add your SSH key to GitHub')
+    }
+    return value
+  }
+  await promptly.confirm('Did you follow all given instructions and successfully added your SSH key (y/n)?', {validator})
 }
