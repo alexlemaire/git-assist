77a4e6bbecc374a025dee5bfff5ef6af1c0a3dcb
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-03-23 13:15:45 +0100

    Testing upload action

--- .github/workflows/scripts/build-archive.sh
+++ .github/workflows/scripts/build-archive.sh
@@ -4,5 +4,6 @@ cd easy-use
 zip -r easy-use.zip .
 git config --global user.name Alexis Lemaire
 git config --global user.email a.lemaire@klimapartner.de
+git add easy-use.zip
 git commit -am "Built easy-use.zip archive"
 git push
