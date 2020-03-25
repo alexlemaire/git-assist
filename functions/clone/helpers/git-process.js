d4a0a6dadebacc692e5a8f9d4aba1cf5e151dafe
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-03-25 12:12:13 +0100

    *minor* Added clone utility

---
+++ functions/clone/helpers/git-process.js
@@ -0,0 +1,35 @@
+module.exports = async (protocol, url, repo) => {
+  const clog = require('../../../utils/loggers/console-log.js')
+  clog.info(`Cloning ${repo} into current folder...`)
+  switch (protocol) {
+    case 'ssh':
+      sshClone(url)
+      break
+    case 'https':
+      await httpsClone(url, repo)
+      break
+    default:
+      break
+  }
+  clog.success(`${repo} successfully cloned!`)
+}
+
+async function httpsClone(url, repo) {
+  const fs = require('fs')
+  const http = require('isomorphic-git/http/node')
+  const git = require('isomorphic-git')
+  const path = require('path')
+  await git.clone({
+    fs,
+    http,
+    dir: path.join(process.cwd(), repo),
+    url,
+    depth: 1
+  })
+}
+
+function sshClone(url) {
+  // isomorphic-git doesn't seem to handle SSH cloning just yet so we use regular git command for now. Same as in push utility
+  const spawnSync = require('child_process').spawnSync
+  spawnSync('git', ['clone', url])
+}
