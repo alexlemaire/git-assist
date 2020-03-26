912a17027afe3edf587b1ebd6ab3b9d25de4a870
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-03-26 14:43:31 +0100

    Fixed push utility with https

---
+++ functions/push/helpers/git-process/commit.js
@@ -0,0 +1,11 @@
+module.exports = async (message) => {
+  // not using isomorphic-git here because the signing plugin has some security issues
+  const spawnSync = require('child_process').spawnSync
+  const clog = require('../../../../utils/loggers/console-log.js')
+  clog.info('If required, please input your passphrase for your GPG key in order to sign your commit.')
+  spawnSync('git', [
+    'commit',
+    '-S',
+    '-m', message
+  ])
+}
