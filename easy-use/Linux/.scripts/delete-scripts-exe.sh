f49c7febfb7eaa05dece9742cfdf36dc21da0f81
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-04-03 16:53:31 +0200

    Added delete-scripts easy-use script

---
+++ easy-use/Linux/.scripts/delete-scripts-exe.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+INFO="$(cat .info)"
+
+if [ $INFO != "root" ]
+then
+  FOLDER="$(basename "$(pwd)")"
+  cd ../
+  echo "Removing easy-use scripts directory..."
+  rm -r $FOLDER
+  sed -i "/# Ignore folder for easy-use scripts from git-assist package/d" .gitignore
+  sed -i "/$FOLDER\//d" .gitignore
+  echo "Done! You can now close this window."
+else
+  echo "Cannot delete easy-use scripts from original folder!"
+fi
