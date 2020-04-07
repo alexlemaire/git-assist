566338082f331b813a4beb9859db0e5779f094a8
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-04-07 11:37:51 +0200

    Added password storage to the authentication helpers

--- src/utils/auth/auth.js
+++ src/utils/auth/auth.js
@@ -1,16 +1,23 @@
+const clog = require('../loggers/console-log.js')
+const chalk = require('chalk')
+const pwdManager = require('./pwd-manager.js')
+
 module.exports = async () => {
-  const inquirer = require('inquirer')
-  const questions = [
-    {
-      type: 'input',
-      name: 'username',
-      message: 'GitHub username:'
-    },
-    {
-      type: 'password',
-      name: 'password',
-      message: 'GitHub password (access token if using MFA):'
-    }
-  ]
-  return await inquirer.prompt(questions)
+  const username = await pwdManager.getUsername()
+  clog.info(`Pulled username ${chalk.green.italic(username)} from local configuration file. If you would like to work under another identity, please update your local configuration file (with ${chalk.blue.italic('git-assist config')})`, {makeLink: false, format: false})
+  let password = await pwdManager.getPwd(username)
+  if (!password) {
+    { password } = await pwdManager.promptPwd()
+    await pwdManager.setPwd(username, password)
+  }
+  return { username, password }
+}
+
+async function getUsername() {
+  const git = require('isomorphic-git')
+  return await git.getConfig({
+    fs: require('fs'),
+    dir: '.',
+    path: 'user.email'
+  })
 }
