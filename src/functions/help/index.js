d7921099806891c4c2e23b39ec2cc2781ff35383
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-04-08 13:57:14 +0200

    Updated help utility to give clearer help + returns proper help per fcts

--- src/functions/help/index.js
+++ src/functions/help/index.js
@@ -1,34 +1,39 @@
+const clog = require('../../utils/loggers/console-log.js')
 const chalk = require('chalk')
 
 module.exports = (args) => {
-  const fs = require('fs')
-  const clog = require('../../utils/loggers/console-log.js')
   let fcts = require('../../../functions.json')
   delete fcts['help']
   require('../../utils/welcome/welcome.js')
-  clog.info(`To call any of the functions listed below, you can run ${chalk.cyan.italic('git-assist <function_name> [accepted_options]')}`, {
-    makeLink: false,
-    format: false
-  })
-  console.log('\n')
   if (args.length === 0) {
-    for (const entry of Object.entries(fcts)) {
-      logInfo(entry)
-    }
+    globalHelp(Object.entries(fcts))
+    clog.info(`To get help for a specific function, please use ${chalk.italic.cyan('git-assist [-h, --help] <function_name>')}\n`, {makeLink: false, format: false})
   } else {
-    logInfo([args[0], fcts[args[0]]])
+    help([args[0], fcts[args[0]]])
   }
+}
+
+function globalHelp (entries) {
+  clog.info('Printing help for all available functions...\n')
+  for (const entry of entries) {
+    logInfo(entry)
+  }
+}
+
+function help (entry) {
+  clog.info(`Printing help for ${chalk.green.italic(entry[0])} function...\n`)
+  logInfo(entry)
 }
 
 function logInfo(entry) {
   console.log(`${chalk.yellow('➢')} ${chalk.yellow.underline('Function')}: ${entry[0]}`)
   console.log(`  ${chalk.blue.underline('Description')}: ${entry[1].desc}`)
-  console.log(`  ${chalk.cyan.underline('Command')}: ${chalk.italic(`git-assist ${entry[0]} ${entry[1].optsData.length > 0 ? `${entry[1].optsData.map(optData => `[${optData.opts.join(', ')}]`).join(' ')}` : ''}`)}`)
+  console.log(`  ${chalk.cyan.underline('Command')}: ${chalk.italic.magenta(`git-assist ${entry[0]} ${entry[1].optsData.map(optData => `[${optData.opts.join(', ')}]`).join(' ')}`.trim())}`)
   for (const optData of entry[1].optsData) {
     if (entry[1].optsData.indexOf(optData) === 0) {
       console.log(`  ${chalk.green.underline('Accepted options')}:`)
     }
-    console.log(`    ${chalk.magenta.bold(`• "${optData.opts.join(', ')}"`)}: ${chalk.italic(optData.desc)}`)
+    console.log(`    ${chalk.magenta.bold.italic(`• ${optData.opts.join(', ')}`)}: ${optData.desc}`)
   }
   console.log('\n')
 }
