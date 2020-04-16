4323b0c372cc08b3a4537eebcda6734f5b5697f1
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-04-16 17:12:59 +0200

    auto-pull configurator (WIP)

--- src/functions/auto-pull/helpers/conf-prompter.js
+++ src/functions/auto-pull/helpers/conf-prompter.js
@@ -1,29 +1,27 @@
+const inquirer = require('inquirer')
+const fs = require('fs')
+inquirer.registerPrompt('fuzzypath', require('inquirer-fuzzy-path'))
+
 module.exports = async () => {
-  const inquirer = require('inquirer')
-  // inquirer.registerPrompt('fuzzypath', require('inquirer-fuzzy-path'))
-  // const questions = [
-  //   {
-  //     type: 'fuzzypath',
-  //     name: 'path',
-  //     excludePath: nodePath => nodePath.startsWith('node_modules'),
-  //     excludeFilter: nodePath => nodePath == '.',
-  //     itemType: 'directory',
-  //     rootPath: '/',
-  //     message: 'Path to the directory where you would like to setup auto-pulling:',
-  //     suggestOnly: false
-  //   }
-  // ]
-  const questions = [
-    {
-      type: 'input',
-      name: 'name',
-      message: 'Name:'
-    },
-    {
-      type: 'input',
-      name: 'age',
-      message: 'Age:'
-    }
-  ]
-  return await inquirer.prompt(questions)
+  const { path } = await getPath()
+
+  return {path}
+}
+
+async function getPath() {
+  return await inquirer.prompt([{
+    type: 'fuzzypath',
+    name: 'path',
+    excludePath: nodePath => nodePath.includes('node_modules') || fs.existsSync(`${nodePath}/.git`),
+    itemType: 'directory',
+    rootPath: process.cwd(),
+    message: 'Path to the directory where you would like to setup auto-pulling:',
+    suggestOnly: false,
+    depthLimit: 5
+  }])
+}
+
+async function getExcludedDirs (path) {
+  // this should retrieve dirs to exclude via a checkbox prompt
+  // will need an helper function to list all repos at every level beyond the path specified by user
 }
