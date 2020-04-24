89e8e8f87d7f899e4b75baa0df294d6f8622a5ec
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-04-24 11:28:16 +0200

    Added easy-use scripts for logs utility

---
+++ easy-use/Mac/logs-watch.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+back_if() {
+  if [ "$(basename "$(pwd)")" == $1 ]
+  then
+    cd ../
+  fi
+}
+back_if "Linux"
+back_if "easy-use"
+
+header_print() {
+  echo $1
+  echo "Tip: you can cancel this operation by pressing CTRL + C"
+}
+
+EXEC="$(npm bin -g)/git-assist"
+
+# Pulling
+header_print "Starting logs printing utility..."
+$EXEC logs --watch
+
+echo "Done!"
+read -p "Press Return to exit"
