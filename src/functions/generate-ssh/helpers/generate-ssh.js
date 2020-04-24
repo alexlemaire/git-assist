e33cb5c725cf5393c688d84e4fec976085a2f655
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-04-24 13:10:48 +0200

    Updated generate-ssh to handle windows path style + non existing folders

--- src/functions/generate-ssh/helpers/generate-ssh.js
+++ src/functions/generate-ssh/helpers/generate-ssh.js
@@ -1,5 +1,6 @@
 module.exports = (info) => {
   const spawnSync = require('child_process').spawnSync
+  createFolder(info.path)
   clog.info('Generating SSH key...')
   spawnSync('ssh-keygen', [
     '-t', 'rsa',
@@ -9,4 +10,19 @@ module.exports = (info) => {
     '-f', info.path
   ])
   clog.success('SSH key generated!')
+}
+
+function createFolder(folderPath) {
+  const fs = require('fs')
+  const path = require('path')
+  folderPath = path.dirname(folderPath)
+  let folderPaths = folderPath.split(path.sep).map((pathFrag, index, array) => path.sep + path.join(...array.slice(0, index + 1)))
+  folderPaths.shift()
+  for (const folderPath of folderPaths) {
+    if (!fs.existsSync(folderPath)) {
+      clog.info(`Folder ${folderPath} does not exists, creating...`)
+      fs.mkdirSync(folderPath)
+      clog.success('Done!')
+    }
+  }
 }
--- src/functions/generate-ssh/helpers/info-prompter.js
+++ src/functions/generate-ssh/helpers/info-prompter.js
@@ -1,34 +1,48 @@
-const questions = [
-  {
-    type: 'input',
-    name: 'email',
-    message: 'GitHub email:'
-  },
-  {
-    type: 'input',
-    name: 'path',
-    message: 'Enter file in which to save the key:',
-    default: `${process.env.HOME}/.ssh/id_rsa`
-  },
-  {
-    type: 'password',
-    name: 'pwd',
-    message: 'Enter a password:'
-  },
-  {
-    type: 'password',
-    name: 'confpwd',
-    message: 'Enter password again:',
-    validate: function (input, answer) {
-      if (input !== answer.pwd) {
-        throw new Error('\nError: passwords must match!\n')
-      }
-      return true
-    }
-  }
-]
-
 module.exports = async () => {
   const inquirer = require('inquirer')
-  return await inquirer.prompt(questions)
+  return await inquirer.prompt(getQuestions())
+}
+
+function getQuestions() {
+  return [
+    {
+      type: 'input',
+      name: 'email',
+      message: 'GitHub email:'
+    },
+    {
+      type: 'input',
+      name: 'path',
+      message: 'Enter file in which to save the key:',
+      default: getDefaultPath(),
+      filter: processPath
+    },
+    {
+      type: 'password',
+      name: 'pwd',
+      message: 'Enter a password:'
+    },
+    {
+      type: 'password',
+      name: 'confpwd',
+      message: 'Enter password again:',
+      validate: validatePwd
+    }
+  ]
+}
+
+function getDefaultPath() {
+  const path = require('path')
+  return path.join(process.env.HOME, '.ssh', 'id_rsa')
+}
+
+function processPath(input) {
+  return input.replace('~', process.env.HOME)
+}
+
+function validatePwd(input, answer) {
+  if (input !== answer.pwd) {
+    throw new Error('\nError: passwords must match!\n')
+  }
+  return true
 }
