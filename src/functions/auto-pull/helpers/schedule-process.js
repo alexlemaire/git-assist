66834e8953599ddd24d53b5733a37588c8de4fe1
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-04-27 18:22:40 +0200

    Implemented auto-pull scheduling feature - few improvements missing

--- src/functions/auto-pull/helpers/schedule-process.js
+++ src/functions/auto-pull/helpers/schedule-process.js
@@ -1,49 +1,90 @@
 const chalk = require('chalk')
+let startConf
 
 module.exports = async (opts) => {
   if (process.platform === 'win32') {
     throw new Error(`Apologies, scheduling ${chalk.italic.blue('auto-pull')} is currently not supported on Windows...`)
   }
-  if (opts.scheduled) {
-    clog.info('WARNING: this feature is kind of experimental. It may not work on some OS or on your machine. Feel free to file any bugs you encounter! (I did not have many machines to try this on and since PM2 is already setup on mines I am not sure how it will work on a machine without prior configuration...)')
-    clog.info(`Scheduling ${chalk.italic.blue('auto-pull')}...`)
-    await daemonize(opts)
-    clog.success(`${chalk.italic.blue('auto-pull')} scheduled!`)
+  if (!opts.scheduled) {
+    return
   }
+  clog.info('WARNING: this feature is kind of experimental. It may not work on some OS or on your machine. Feel free to file any bugs you encounter! (I did not have many machines to try this on and since PM2 is already setup on mines I am not sure how it will work on a machine without prior configuration...)')
+  clog.info(`Scheduling ${chalk.italic.blue('auto-pull')}...`)
+  await processAction(opts)
+  clog.success(`${chalk.italic.blue('auto-pull')} scheduling successfully updated!`)
 }
 
-function daemonize (opts) {
+async function processAction(opts) {
   if (process.getuid() !== 0) {
     throw new Error(`You must run this command with elevated rights... This command is working with ${chalk.italic.cyan('pm2')} to setup ${chalk.italic.cyan('auto-pull')} so that it restarts when your machine restarts.\nPlease run ${chalk.italic.blue('sudo git-assist auto-pull [-c, --config]')} instead.`)
   }
-  const startConf = {
-    name: 'scheduled-auto-pull',
+  startConf = {
     script: `${appRoot}/index.js`,
     args: 'auto-pull',
     cron_restart: opts.cron,
     max_memory_restart : '100M',
     autorestart: false
   }
-  promisifyPm2('connect')
-  .then(res => promisifyPm2('start', startConf))
-  .then(res => promisifyPm2('startup', null, {}))
-  .then(res => promisifyPm2('dump'))
-  .then(res => promisifyPm2('disconnect'))
+  switch (opts.action) {
+    case 'add':
+      clog.info(`Adding ${chalk.italic.blue(opts.type)} scheduled run for ${chalk.italic.cyan('auto-pull')}...`)
+      await addProcess(opts)
+      break
+    case 'edit':
+      clog.info(`Editing ${chalk.italic.blue(opts.name)} process...`)
+      await editProcess(opts)
+      break
+    case 'delete':
+      clog.info(`Deleting ${chalk.italic.blue(opts.name)} process...`)
+      await deleteProcess(opts)
+      break
+  }
+}
+
+async function addProcess(opts) {
+  startConf.name = `${opts.type}-auto-pull`
+  await pm2Update([
+    {
+      method: 'start',
+      params: [startConf]
+    }
+  ])
+}
+
+async function deleteProcess(opts) {
+  await pm2Update([
+    {
+      method: 'delete',
+      params: [opts.name]
+    }
+  ])
+}
+
+async function editProcess(opts) {
+  startConf.name = opts.name
+  await pm2Update([
+    {
+      method: 'delete',
+      params: [opts.name]
+    },
+    {
+      method: 'start',
+      params: [startConf]
+    }
+  ])
+}
+
+async function pm2Update(commands) {
+  const pm2 = require(appRoot + '/src/utils/pm2/pm2-utils.js')
+  await pm2.connect()
+  for (const command of commands) {
+    await pm2[command.method](...command.params)
+  }
+  await pm2.startup(null, {})
+  .then(res => pm2.dump())
+  .then(res => pm2.disconnect())
   .catch(err => {
     clog.error('There was an error with PM2...')
     throw new Error(err)
-  })
-}
-
-function promisifyPm2(method, ...params) {
-  const pm2 = require('pm2')
-  return new Promise((resolve, reject) => {
-    pm2[method](...params, function(err, data) {
-      if (err) {
-        reject(err)
-      } else {
-        resolve(data)
-      }
-    })
   })
 }
--- src/functions/auto-pull/helpers/schedule-prompter.js
+++ src/functions/auto-pull/helpers/schedule-prompter.js
@@ -1,16 +1,62 @@
+const inquirer = require('inquirer')
+const chalk = require('chalk')
+
 module.exports = async () => {
-  const inquirer = require('inquirer')
+  const processes = await require('./get-processes.js')()
+  const { action } = await getAction(processes)
+  let res = {
+    scheduled: true
+  }
+  switch (action) {
+    case 'nothing':
+      res.scheduled = false
+      break
+    case 'add':
+      const addAnswer = await addActionPrompt(processes)
+      if (!addAnswer.type) {
+        clog.info(`There are no scheduled ${chalk.italic.cyan('auto-pull')} runs you can add for this machine.`)
+        res.scheduled = false
+      }
+      res = {...res, action, ...addAnswer}
+      break
+    case 'edit':
+    case 'delete':
+      const editAnswer = await editActionPrompt(processes, action)
+      if (!editAnswer.name) {
+        clog.info(`There are no scheduled ${chalk.italic.cyan('auto-pull')} runs you can ${action} for this machine.`)
+        res.scheduled = false
+      }
+      res = {...res, action, ...editAnswer}
+      break
+  }
+  return res
+}
+
+async function getAction(processes) {
+  const subChoices = processes.length > 0 ? ['edit', 'delete'] : []
   const questions = [
     {
-      type: 'confirm',
-      name: 'scheduled',
-      message: 'Would you like to schedule auto-pull for automatic runs?'
-    },
+      type: 'rawlist',
+      name: 'action',
+      message: 'You can also schedule auto-pull to run automatically. What would you like to do?',
+      choices: ['add', ...subChoices, 'nothing']
+    }
+  ]
+  return await inquirer.prompt(questions)
+}
+
+async function addActionPrompt(processes) {
+  const processTypes = processes.map(process => process.replace(/-auto-pull/g, ''))
+  const choices = ['startup', 'cron'].filter(choice => !processTypes.includes(choice))
+  const questions = [
     {
       type: 'rawlist',
       name: 'type',
-      message: 'How would you like to schedule auto-pull?',
-      choices: ['startup', 'cron']
+      message: 'What kind of scheduled run would you like to add?',
+      choices,
+      when: function (answer) {
+        return choices.length > 0
+      }
     },
     {
       type: 'input',
@@ -19,6 +65,37 @@ module.exports = async () => {
       default: '0 0 * * 1',
       when: function (answer) {
         return answer.type === 'cron'
+      }
+    }
+  ]
+  return await inquirer.prompt(questions)
+}
+
+async function editActionPrompt(processes, action) {
+  let choices = processes
+  if (action === 'edit') {
+    choices = choices.filter(choice => choice.replace(/-auto-pull/g, '') !== 'startup')
+  }
+  const questions = [
+    {
+      type: 'rawlist',
+      name: 'name',
+      message: `Select a process you would like to ${action}?`,
+      choices,
+      when: function (answer) {
+        return choices.length > 0
+      }
+    },
+    {
+      type: 'input',
+      name: 'cron',
+      message: 'Please enter a new cron pattern for your auto-pull scheduled runs:',
+      default: '0 0 * * 1',
+      when: function (answer) {
+        if (!answer.name) {
+          return false
+        }
+        return answer.name.startsWith('cron') && action === 'edit'
       }
     }
   ]
