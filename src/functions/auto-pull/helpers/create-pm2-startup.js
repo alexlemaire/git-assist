90f2131e3265b82c2578ed3a4a0dced78bb277c2
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-04-28 12:49:19 +0200

    Added pm2 global version check + warning

--- src/functions/auto-pull/helpers/create-pm2-startup.js
+++ src/functions/auto-pull/helpers/create-pm2-startup.js
@@ -1,6 +1,8 @@
 const childProc = require('child_process')
 
 module.exports = async (config) => {
+  const localVer = require(appRoot + '/package.json').dependencies.pm2.replace('^', '')
+  checkGlobalVer(localVer)
   if (process.version === config.get('node_version') && config.get('startup_ran')) {
     clog.info('No need to create pm2 startup script for this machine.')
     return
@@ -10,10 +12,9 @@ module.exports = async (config) => {
     clog.info('Aborting process...')
     process.exit()
   }
-  const pm2Version = require(appRoot + '/package.json').dependencies.pm2
-  if (pm2Version !== config.get('pm2_version')) {
+  if (localVer !== config.get('pm2_version')) {
     childProc.spawnSync('node', [`${appRoot}/node_modules/pm2/bin/pm2`, 'update'])
-    config.set('pm2_version', pm2Version)
+    config.set('pm2_version', localVer)
   }
   clog.info('Creating pm2 startup script for this machine...')
   startup(config)
@@ -29,6 +30,14 @@ async function promptConfirm() {
       message: 'About to unstartup/startup pm2 and potentially update the in-memory process. If you are already using pm2, you will need to restart all your processes and save them again. Are you sure you want to proceed?'
     }
   ])
+}
+
+function checkGlobalVer(localVer) {
+  const chalk = require('chalk')
+  const globalVer = childProc.spawnSync('pm2', ['-v']).stdout.toString().trim()
+  if (globalVer.match(/(\d+)\.(\d+)\.(\d+)/) && globalVer !== localVer) {
+    clog.info(`${chalk.underline('WARNING')}: your globally installed ${chalk.italic.cyan('pm2')} version is ${globalVer}. The locally used version of ${chalk.italic.cyan('pm2')} is ${localVer}. You may want to update your globally installed ${chalk.italic.cyan('pm2')} version to avoid conflict of in-memory process versions...`)
+  }
 }
 
 function startup(config) {
