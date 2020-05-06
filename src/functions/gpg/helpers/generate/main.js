fc8a349ada7bfb1a132911b01daae9bbb4e3beaf
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-05-06 16:29:46 +0200

    Added tip on automatic GPG key addition after generation

--- src/functions/gpg/helpers/generate/main.js
+++ src/functions/gpg/helpers/generate/main.js
@@ -1,7 +1,9 @@
 module.exports = async () => {
+  const chalk = require('chalk')
   const info = await require('./info-prompter.js')()
   const keyId = require('./generate-gpg.js')(info)
   await require('./user-info.js')(getKeyASCII(keyId))
+  clog.info(`Your newly generated key will now automatically be added to your GitHub configuration if you run ${chalk.italic.cyan('git-assist config [-g, --global]')} to configure user ${chalk.italic.blue(info.email)}!`)
 }
 
 function getKeyASCII(keyId) {
