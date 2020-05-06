a255d49d5d8d5778d69f7d136b89eed5fb3c7ea4
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-05-06 12:49:43 +0200

    Removed non necessary methods from SSH utility

--- src/functions/ssh/helpers/add/add-key.js
+++
@@ -1,6 +0,0 @@
-module.exports = (user, key) => {
-  const chalk = require('chalk')
-  clog.info(`Updating SSH key for ${chalk.italic.blue(user)}...`)
-  require(appRoot + '/src/utils/key-gen/update-config.js')('ssh', user, key)
-  clog.success('SSH key successfully updated!')
-}
--- src/functions/ssh/helpers/delete/delete-pwd.js
+++
@@ -1,8 +0,0 @@
-module.exports = async (keyPath) => {
-  const chalk = require('chalk')
-  const path = require('path')
-  const pwdManager = require(appRoot + '/src/utils/auth/pwd-manager.js')
-  clog.info(`Deleting password for ${chalk.italic.cyan(path.basename(keyPath))}...`)
-  await pwdManager.deletePwd(keyPath)
-  clog.success(`Successfully deleted password!`)
-}
